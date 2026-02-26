4f764a8a050a9c48ad83df2c1c7123e0a2e31cdc
Uwonsang <u.wonsang0514@gmail.com>
2026-02-25 23:20:51 -0500

    Hot_fix : bash_ippo_pop.sh

--- baselines/CEC/shell/bash_ippo_pop.sh
+++ baselines/CEC/shell/bash_ippo_pop.sh
@@ -3,9 +3,10 @@
 # export XLA_PYTHON_CLIENT_MEM_FRACTION=0.6
 
 layout=$1
+gpu=$2
 SEEDS=(0 1 2 3 4 5)
 for seed in "${SEEDS[@]}"; do
-    CUDA_VISIBLE_DEVICES=1 python baselines/CEC/ippo_general_population.py \
+    CUDA_VISIBLE_DEVICES=${gpu} python baselines/CEC/ippo_general_population.py \
       ENV_KWARGS.layout=${layout} \
       SEED=$seed \
       WANDB_MODE=online

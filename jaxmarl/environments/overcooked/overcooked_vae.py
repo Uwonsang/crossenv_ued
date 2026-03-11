51e99e821c28bafb8a7f7214eada1c3353eda605
Uwonsang <u.wonsang0514@gmail.com>
2026-03-11 14:19:31 +0900

    Update : add initiablize_env in overcooked vae

--- jaxmarl/environments/overcooked/overcooked_vae.py
+++ jaxmarl/environments/overcooked/overcooked_vae.py
@@ -1108,18 +1108,68 @@ if __name__ == "__main__":
     from jaxmarl.environments.overcooked import overcooked_layouts
     import os
     import time
-    xpid = "lr-%s" % time.strftime("%Y%m%d-%H%M")
+    import jaxmarl
+    from omegaconf import OmegaConf
+    from jax_tqdm import scan_tqdm
+    from jaxmarl.environments.overcooked.layouts import make_counter_circuit_9x9, make_forced_coord_9x9, make_coord_ring_9x9, make_asymm_advantages_9x9, make_cramped_room_9x9
 
-    env_kwargs = {
-        'layout': overcooked_layouts["cramped_room_9"],
-        'random_reset': True,
-        'max_steps': 256,
-        'single_agent': False,
-        'check_held_out': False,
-        'shuffle_inv_and_pot': False
-    }
-    # env_kwargs = filter_kwargs(env_kwargs, Overcooked)
-    env = Overcooked_VAE(**env_kwargs)
+    _config_path = "/app/baselines/CEC_UED/config/ippo_overcooked_CEC_VAE.yaml"
+    config = OmegaConf.load(_config_path)
+    config = OmegaConf.to_container(config, resolve=True)
+
+    def initialize_environment(config):
+        layout_name = config["ENV_KWARGS"]["layout"]
+        config['layout_name'] = layout_name
+        config["ENV_KWARGS"]["layout"] = overcooked_layouts[layout_name]
+        env = jaxmarl.make(config["ENV_NAME"], **config["ENV_KWARGS"])
+
+        def reset_env(key):
+            def reset_sub_dict(key, fn):
+                key, subkey = jax.random.split(key)
+                sampled_layout_dict = fn(subkey, ik=True)
+                temp_o, temp_s = env.custom_reset(key, layout=sampled_layout_dict, random_reset=False, shuffle_inv_and_pot=False)
+                key, subkey = jax.random.split(key)
+                return (temp_o, temp_s), key
+                
+            asymm_reset, key = reset_sub_dict(key, make_asymm_advantages_9x9)
+            coord_ring_reset, key = reset_sub_dict(key, make_coord_ring_9x9)
+            counter_circuit_reset, key = reset_sub_dict(key, make_counter_circuit_9x9)
+            forced_coord_reset, key = reset_sub_dict(key, make_forced_coord_9x9)
+            cramped_room_reset, key = reset_sub_dict(key, make_cramped_room_9x9)
+            layout_resets = [asymm_reset, coord_ring_reset, counter_circuit_reset, forced_coord_reset, cramped_room_reset]
+            # stack all layouts
+            stacked_layout_reset = jax.tree_map(lambda *x: jnp.stack(x), *layout_resets)
+            # sample an index from 0 to 4
+            index = jax.random.randint(key, (), minval=0, maxval=5)
+            sampled_reset = jax.tree_map(lambda x: x[index], stacked_layout_reset)
+            return sampled_reset
+        @scan_tqdm(100)
+        def gen_held_out(runner_state, unused):
+            (i,) = runner_state
+            _, ho_state = reset_env(jax.random.key(i))
+            res = (ho_state.goal_pos, ho_state.wall_map, ho_state.pot_pos)
+            carry = (i+1,)
+            return carry, res
+        carry, res = jax.lax.scan(gen_held_out, (0,), jnp.arange(100), 100)
+        ho_goal, ho_wall, ho_pot = [], [], []
+        for layout_name, layout_dict in overcooked_layouts.items():  # add hand crafted ones to heldout set
+            if "9" in layout_name:
+                _, ho_state = env.custom_reset(jax.random.PRNGKey(0), random_reset=False, shuffle_inv_and_pot=False, layout=layout_dict)
+                ho_goal.append(ho_state.goal_pos)
+                ho_wall.append(ho_state.wall_map)
+                ho_pot.append(ho_state.pot_pos)
+        ho_goal = jnp.stack(ho_goal, axis=0)
+        ho_wall = jnp.stack(ho_wall, axis=0)
+        ho_pot = jnp.stack(ho_pot, axis=0)
+        ho_goal = jnp.concatenate([res[0], ho_goal], axis=0)
+        ho_wall = jnp.concatenate([res[1], ho_wall], axis=0)
+        ho_pot = jnp.concatenate([res[2], ho_pot], axis=0)
+        env.held_out_goal, env.held_out_wall, env.held_out_pot = (ho_goal, ho_wall, ho_pot)
+        return env
+    
+    
+    xpid = "lr-%s" % time.strftime("%Y%m%d-%H%M")
+    env = initialize_environment(config)
 
     from jaxmarl.viz.overcooked_jitted_visualizer import render_fn
     import imageio
@@ -1130,11 +1180,11 @@ if __name__ == "__main__":
 
     params={'random_reset_fn': 'reset_vae',
             'vae_decoder_params': decoder_params, 'vae_config': ckpt_config} # reset_all or reset_counter_circuit, reset_coord_ring
-    keys = jax.random.split(jax.random.PRNGKey(0), 30)
+    keys = jax.random.split(jax.random.PRNGKey(0), 100)
     def render_reset(key):
         obs, state = env.reset(key, params=params)
         return render_fn(state)
-    images = jax.vmap(render_reset)(keys)
+    images = jax.jit(jax.vmap(render_reset))(keys)
     # for each image, save it as a png
     save_path = "/app/jaxmarl/environments/overcooked/images_test"
     os.makedirs(save_path, exist_ok=True)

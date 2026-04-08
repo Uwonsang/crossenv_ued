716f573f1dde10f5aa989b0b0c6208a96bea4ffc
Uwonsang <u.wonsang0514@gmail.com>
2026-04-08 13:29:30 +0900

    update minimax_yaml

--- baselines/CEC_UED/ippo_general_minimax.py
+++ baselines/CEC_UED/ippo_general_minimax.py
@@ -821,7 +821,7 @@ def main(config):
         tags=["IPPO", "RNN", "SP"],
         config=config,
         mode=config["WANDB_MODE"],
-        name=f"CEC_{layout_name}_seed{config['SEED']}"
+        name=f"CEC_minimax_{layout_name}_seed{config['SEED']}"
     )
     filepath = f"ckpts/ippo/{config['ENV_NAME']}"
     if config["ENV_NAME"] == "overcooked":

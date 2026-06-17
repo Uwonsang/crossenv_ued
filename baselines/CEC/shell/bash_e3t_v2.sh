0c16caba960af12e55d0e06104cf47c336b469d5
Uwonsang <uwonsang0514@gmail.com>
2026-06-17 14:46:57 +0900

    update e3t

--- baselines/CEC/shell/bash_e3t_v2.sh
+++ baselines/CEC/shell/bash_e3t_v2.sh
@@ -21,22 +21,22 @@ CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=coord_ring_
 CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
 CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
 CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=asymm_advantages_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
 
-# forced_coord_9
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# # forced_coord_9
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=forced_coord_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
 
-counter_circuit_9
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
-CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# # counter_circuit_9
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64
+# CUDA_VISIBLE_DEVICES=0 python baselines/CEC/e3t.py ENV_KWARGS.layout=counter_circuit_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2 NUM_ENVS=64

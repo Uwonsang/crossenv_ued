9df00e7407f08b00d475f096e9e5f447c1d33f65
Lee Joo Yub <guylee97@naver.com>
2026-08-11 01:36:23 +0900

    fix: sh fix

--- baselines/CEC_UED/shell/idaac_pop_seeds_3_5.sh
+++ baselines/CEC_UED/shell/idaac_pop_seeds_3_5.sh
@@ -2,6 +2,7 @@
 set -e
 
 cd "$(dirname "$0")/../../.."
+export PYTHONPATH="$PWD:${PYTHONPATH:-}"
 
 for seed in 3 4 5; do
   python3 baselines/CEC_UED/idaac_general_gradient_pop.py SEED="$seed" WANDB_MODE=online

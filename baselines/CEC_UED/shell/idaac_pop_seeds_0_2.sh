568cf21eb2f9a41a0aba14a1b8cb04fb450cf1ad
Lee Joo Yub <guylee97@naver.com>
2026-08-11 16:15:35 +0900

    feat : idaac 학습 추가

--- baselines/CEC_UED/shell/idaac_pop_seeds_0_2.sh
+++ baselines/CEC_UED/shell/idaac_pop_seeds_0_2.sh
@@ -4,6 +4,8 @@ set -e
 cd "$(dirname "$0")/../../.."
 export PYTHONPATH="$PWD:${PYTHONPATH:-}"
 
+python3 baselines/CEC_UED/modified_wall_idaac_general_gradient_pop.py SEED=98 NUM_ENVS=256 XP_KWARGS.enabled=false WANDB_MODE=online
+
 for seed in 0 1 2; do
-  python3 baselines/CEC_UED/idaac_general_gradient_pop.py SEED="$seed" WANDB_MODE=online
+  python3 baselines/CEC_UED/modified_wall_idaac_general_gradient_pop.py SEED="$seed" NUM_ENVS=256 XP_KWARGS.enabled=true XP_KWARGS.partner_seed=98 WANDB_MODE=online
 done

42e1e7f8091c10453f3569d9b95ac0835cde4b90
Lee Joo Yub <guylee97@naver.com>
2026-08-11 18:44:37 +0900

    fix

--- baselines/CEC_UED/shell/idaac_pop_seeds_0_2.sh
+++ baselines/CEC_UED/shell/idaac_pop_seeds_0_2.sh
@@ -4,8 +4,6 @@ set -e
 cd "$(dirname "$0")/../../.."
 export PYTHONPATH="$PWD:${PYTHONPATH:-}"
 
-python3 baselines/CEC_UED/modified_wall_idaac_general_gradient_pop.py SEED=98 NUM_ENVS=256 XP_KWARGS.enabled=false WANDB_MODE=online
-
 for seed in 0 1 2; do
-  python3 baselines/CEC_UED/modified_wall_idaac_general_gradient_pop.py SEED="$seed" NUM_ENVS=256 XP_KWARGS.enabled=true XP_KWARGS.partner_seed=98 WANDB_MODE=online
+  python3 baselines/CEC_UED/modified_wall_idaac_general_gradient_pop.py SEED="$seed" NUM_ENVS=256 EVAL_KWARGS.eval_interval=25 XP_KWARGS.enabled=true XP_KWARGS.partner_seed=98 WANDB_MODE=online
 done

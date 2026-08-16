90ead259c252acf561d2b273df1e7ff1865cd8d9
wonsang <u.wonsang0514@gmail.com>
2026-08-16 23:43:51 +0900

    update shell

--- baselines/CEC/shell/bash_ippo_pop2_forced.sh
+++ baselines/CEC/shell/bash_ippo_pop2_forced.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+
+DEFAULT_GPU=3
+GPU_ID="${1:-${CUDA_VISIBLE_DEVICES:-$DEFAULT_GPU}}"
+export CUDA_VISIBLE_DEVICES="$GPU_ID"
+echo "Using GPU $CUDA_VISIBLE_DEVICES"
 #layouts=(cramped_room_9 coord_ring_9 asymm_advantages_9 forced_coord_9 counter_circuit_9)
 # export XLA_PYTHON_CLIENT_MEM_FRACTION=0.6
 
@@ -28,12 +33,12 @@
 
 
 # # forced_coord_9
-CUDA_VISIBLE_DEVICES=3 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2
-CUDA_VISIBLE_DEVICES=3 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2
-CUDA_VISIBLE_DEVICES=3 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2
-CUDA_VISIBLE_DEVICES=3 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2
-CUDA_VISIBLE_DEVICES=3 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2
-CUDA_VISIBLE_DEVICES=3 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2
+python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2
+python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=1 WANDB_MODE=online PROJECT=crossenv_baseline_v2
+python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=2 WANDB_MODE=online PROJECT=crossenv_baseline_v2
+python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=3 WANDB_MODE=online PROJECT=crossenv_baseline_v2
+python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=4 WANDB_MODE=online PROJECT=crossenv_baseline_v2
+python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=forced_coord_9 SEED=5 WANDB_MODE=online PROJECT=crossenv_baseline_v2
 
 # # counter_circuit_9
 # CUDA_VISIBLE_DEVICES=1 python baselines/CEC/ippo_general_population_v2.py ENV_KWARGS.layout=counter_circuit_9 SEED=0 WANDB_MODE=online PROJECT=crossenv_baseline_v2
